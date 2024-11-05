f2a7173666255a3a98de914a85c7897160c133c8
12mativ <ivan_m146@mail.ru>
2024-11-05 23:42:34 +0400

    documentation: add requirements.txt file for easy modules installing

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+class Body(BaseModel):
+  courseDates: dict[str, datetime]
+  courseTheme: str
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-  return {"message": "Hello World"}
+@app.post("/create-shedule")
+async def root(body: Body) -> Body:
+  return body
